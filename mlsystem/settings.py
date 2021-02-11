9a657c98a3830dcdc8d70a72d4d41bd2c70ec1e1
mehran1421 <m.kamrani1421@gmail.com>
2021-02-11 18:12:55 +0330

    login page

--- mlsystem/settings.py
+++ mlsystem/settings.py
@@ -15,6 +15,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'account:list_book'
+LOGOUT_REDIRECT_URL = 'login'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
--- mlsystem/urls.py
+++ mlsystem/urls.py
@@ -18,8 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('django.contrib.auth.urls')),
     path('account/', include('account.urls')),
-    path('book/',include('book.urls')),
 ]
 
 from django.conf import settings
